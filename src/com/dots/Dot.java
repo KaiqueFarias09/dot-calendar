85025abddf7eeea3f29f86992e051eff2f78fba6
Kaique Farias <kaiquesouzafarias@outlook.com>
2023-12-14 19:03:03 -0300

    refactor: :label: transform Dot class into a Record

--- src/com/dots/Dot.java
+++ src/com/dots/Dot.java
@@ -1,19 +1,4 @@
 package com.dots;
 
-public class Dot {
-    private int x;
-    private int y;
-
-    public Dot(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
+public record Dot(int x, int y) {
 }
