73aae32d0209c5f17155642e89aec60a316577bf
Kaique Farias <kaiquesouzafarias@outlook.com>
2023-12-14 12:48:48 -0300

    feat: :sparkles: make dot appear in main dot last coordinate

--- src/com/dots/main.java
+++ src/com/dots/main.java
@@ -1,6 +1,8 @@
 package com.dots;
 
 import java.awt.EventQueue;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
 
 import javax.swing.JFrame;
 
@@ -9,21 +11,24 @@ public class main extends JFrame {
     public main() {
         initUI();
     }
-    
+
     private void initUI() {
         add(new Board());
-               
-        setResizable(false);
         pack();
-        
+
+        Rectangle r = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+
         setTitle("Dynamic Dot Grid Simulation");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(MAXIMIZED_BOTH);
+        setSize(r.width, r.height);
+        setVisible(true);
+        setUndecorated(true);
     }
-    
 
     public static void main(String[] args) {
-        
+
         EventQueue.invokeLater(() -> {
             JFrame ex = new main();
             ex.setVisible(true);
