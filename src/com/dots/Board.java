181c223bfdfb6325da72d56de57f69cbd66430a7
Kaique Farias <kaiquesouzafarias@outlook.com>
2023-12-14 15:27:18 -0300

    feat: :sparkles: add code to remove dots
    add code to remove dots when it get's to the half of a month

--- src/com/dots/Board.java
+++ src/com/dots/Board.java
@@ -25,7 +25,10 @@ public class Board extends JPanel implements ActionListener {
     private final Random random = new Random();
     private List<Dot> dots = new ArrayList<>();
 
-    private boolean inGame = true;
+    private boolean shouldGenerateDots = true;
+    private int day = 0;
+    private int halfOfMonth = 16;
+    private int monthNumber = 1;
 
     private int mainCircleXCoordinate;
     private int mainCircleYCoordinate;
@@ -66,7 +69,7 @@ public class Board extends JPanel implements ActionListener {
 
     private void doDrawing(Graphics g) {
 
-        if (inGame) {
+        if (shouldGenerateDots) {
             drawMainCircle(g);
 
             for (int z = 0; z < dots.size(); z++) {
@@ -88,29 +91,73 @@ public class Board extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (inGame) {
-            if (isCloseToTheBorder()) {
-                mainCircleXCoordinate = generateRandomPosition();
-                mainCircleYCoordinate = generateRandomPosition();
+        if (shouldGenerateDots) {
+
+            if (day != halfOfMonth) {
+                if (isCloseToTheBorder()) {
+                    mainCircleXCoordinate = generateRandomPosition();
+                    mainCircleYCoordinate = generateRandomPosition();
+                } else {
+                    dots.add(new Dot(mainCircleXCoordinate, mainCircleYCoordinate));
+
+                    List<int[]> coordinates = getNeighborCoordinates(mainCircleXCoordinate, mainCircleYCoordinate);
+                    List<int[]> validCoordinates = filterValidCoordinates(coordinates, dots);
+
+                    int randomIndex = random.nextInt(validCoordinates.size());
+                    int[] chosenCoordinate = validCoordinates.get(randomIndex);
+                    mainCircleXCoordinate = chosenCoordinate[0];
+                    mainCircleYCoordinate = chosenCoordinate[1];
+
+                    createNewDotInRandomCoordinate();
+                }
+                day += 1;
             } else {
-                dots.add(new Dot(mainCircleXCoordinate, mainCircleYCoordinate));
+                if (dots.size() > monthNumber) {
+                    Dot dotWithLessNeighbors = findDotWithLeastNeighbors(dots);
+                    dots.remove(dotWithLessNeighbors);
+                }
 
-                List<int[]> coordinates = getNeighborCoordinates(mainCircleXCoordinate, mainCircleYCoordinate);
-                List<int[]> validCoordinates = filterValidCoordinates(coordinates, dots);
-
-                int randomIndex = random.nextInt(validCoordinates.size());
-                int[] chosenCoordinate = validCoordinates.get(randomIndex);
-                mainCircleXCoordinate = chosenCoordinate[0];
-                mainCircleYCoordinate = chosenCoordinate[1];
-
-                createNewDotInRandomCoordination();
             }
 
             repaint();
         }
     }
 
-    private void createNewDotInRandomCoordination() {
+    private Dot findDotWithLeastNeighbors(List<Dot> dots) {
+        int minNeighborCount = Integer.MAX_VALUE;
+        Dot dotWithLeastNeighbors = null;
+
+        for (Dot dot : dots) {
+            int neighborCount = countNeighbors(dot, dots);
+            if (neighborCount < minNeighborCount) {
+                minNeighborCount = neighborCount;
+                dotWithLeastNeighbors = dot;
+            }
+        }
+
+        return dotWithLeastNeighbors;
+    }
+
+    private int countNeighbors(Dot targetDot, List<Dot> dots) {
+        int count = 0;
+
+        for (Dot dot : dots) {
+            if (dot != targetDot && isNeighbor(dot, targetDot)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isNeighbor(Dot dot1, Dot dot2) {
+        int dx = Math.abs(dot1.getX() - dot2.getX());
+        int dy = Math.abs(dot1.getY() - dot2.getY());
+
+        return (dx == MOVE_AMOUNT && dy == 0) || (dx == 0 && dy == MOVE_AMOUNT);
+    }
+
+    private void createNewDotInRandomCoordinate() {
         List<int[]> newCoordinates = getNeighborCoordinates(mainCircleXCoordinate, mainCircleYCoordinate);
         int[] randomCoordinate = newCoordinates.get(random.nextInt(newCoordinates.size()));
         dots.add(new Dot(randomCoordinate[0], randomCoordinate[1]));
