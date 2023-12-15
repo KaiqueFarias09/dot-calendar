7610018376d8baf694a06c05bad0e438ff5b9122
Kaique Farias <kaiquesouzafarias@outlook.com>
2023-12-14 21:09:28 -0300

    refactor: :recycle: reduce cognitive complexity of actionPerformed function
    reduce cognitive complexity of actionPerformed function and fix sonnarlint warnings

--- src/com/dots/Board.java
+++ src/com/dots/Board.java
@@ -1,29 +1,21 @@
 package com.dots;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Toolkit;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
-
-import javax.swing.JPanel;
-import javax.swing.Timer;
 
 public class Board extends JPanel implements ActionListener {
 
-    private final int DOT_SIZE = 10;
-    private final int BOARD_WIDTH = 900 - DOT_SIZE;
-    private final int BOARD_HEIGHT = 900 - DOT_SIZE;
-
+    private static final int DOT_SIZE = 10;
+    private static final int BOARD_WIDTH = 900 - DOT_SIZE;
+    private static final int BOARD_HEIGHT = 900 - DOT_SIZE;
+    private static final boolean SHOULD_GENERATE_DOTS = true;
     private final Random random = new Random();
     private final List<Dot> dots = new ArrayList<>();
-
-    private final boolean shouldGenerateDots = true;
     private int day = 0;
 
     private int mainCircleXCoordinate;
@@ -45,8 +37,7 @@ public class Board extends JPanel implements ActionListener {
     }
 
     private void initGame() {
-        int DELAY = 1000;
-        Timer timer = new Timer(DELAY, this);
+        Timer timer = new Timer(1000, this);
         timer.start();
     }
 
@@ -65,7 +56,7 @@ public class Board extends JPanel implements ActionListener {
 
     private void doDrawing(Graphics g) {
 
-        if (shouldGenerateDots) {
+        if (SHOULD_GENERATE_DOTS) {
             drawMainCircle(g);
 
             for (final Dot dot : dots) {
@@ -86,72 +77,81 @@ public class Board extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (shouldGenerateDots) {
-
+        if (SHOULD_GENERATE_DOTS) {
             int halfOfMonth = 16;
             if (day != halfOfMonth) {
-                if (isCloseToTheBorder()) {
-                    mainCircleXCoordinate = generateRandomPosition();
-                    mainCircleYCoordinate = generateRandomPosition();
-                } else {
-                    dots.add(new Dot(mainCircleXCoordinate, mainCircleYCoordinate));
-
-                    List<int[]> coordinates = getNeighborCoordinates(mainCircleXCoordinate, mainCircleYCoordinate);
-                    List<int[]> validCoordinates = filterValidCoordinates(coordinates, dots);
-
-                    if (!validCoordinates.isEmpty()) {
-                        int randomIndex = random.nextInt(validCoordinates.size());
-                        int[] chosenCoordinate = validCoordinates.get(randomIndex);
-                        mainCircleXCoordinate = chosenCoordinate[0];
-                        mainCircleYCoordinate = chosenCoordinate[1];
-
-                        createNewDotInRandomCoordinate();
-                    } else {
-                        mainCircleXCoordinate = generateRandomPosition();
-                        mainCircleYCoordinate = generateRandomPosition();
-                        createNewDotInRandomCoordinate();
-                    }
-                }
-                day += 1;
+                handleNonHalfOfMonth();
             } else {
-                int monthNumber = 1;
-
-                if (dots.size() > monthNumber) {
-                    Dot dot = DotManager
-                            .getNeighborWithMostNeighbors(new Dot(mainCircleXCoordinate, mainCircleYCoordinate), dots);
-                    if (dot != null) {
-                        dots.remove(dot);
-
-                        mainCircleXCoordinate = dot.x();
-                        mainCircleYCoordinate = dot.y();
-                    } else {
-                        Dot closestDot = DotManager.getClosestDot(new Dot(mainCircleXCoordinate, mainCircleYCoordinate),
-                                dots);
-                        dots.remove(closestDot);
-
-                        mainCircleXCoordinate = closestDot.x();
-                        mainCircleYCoordinate = closestDot.y();
-                    }
-                }
-
-                if (dots.size() > monthNumber) {
-                    Dot dotWithLessNeighbors = DotManager.findDotWithLeastNeighbors(dots);
-                    dots.remove(dotWithLessNeighbors);
-                }
+                handleHalfOfMonth();
             }
-
             repaint();
         }
     }
 
+    private void handleNonHalfOfMonth() {
+        if (isCloseToTheBorder()) {
+            mainCircleXCoordinate = generateRandomPosition();
+            mainCircleYCoordinate = generateRandomPosition();
+        } else {
+            handleNonBorderCase();
+        }
+        day += 1;
+    }
+
+    private void handleNonBorderCase() {
+        dots.add(new Dot(mainCircleXCoordinate, mainCircleYCoordinate));
+
+        List<int[]> coordinates = DotManager.getNeighborCoordinates(mainCircleXCoordinate, mainCircleYCoordinate);
+        List<int[]> validCoordinates = DotManager.filterValidCoordinates(coordinates, dots);
+
+        if (!validCoordinates.isEmpty()) {
+            int randomIndex = random.nextInt(validCoordinates.size());
+            int[] chosenCoordinate = validCoordinates.get(randomIndex);
+            mainCircleXCoordinate = chosenCoordinate[0];
+            mainCircleYCoordinate = chosenCoordinate[1];
+
+            createNewDotInRandomCoordinate();
+        } else {
+            mainCircleXCoordinate = generateRandomPosition();
+            mainCircleYCoordinate = generateRandomPosition();
+            createNewDotInRandomCoordinate();
+        }
+    }
+
+    private void handleHalfOfMonth() {
+        int monthNumber = 1;
+
+        if (dots.size() > monthNumber) {
+            Dot dot = DotManager.getNeighborWithMostNeighbors(new Dot(mainCircleXCoordinate, mainCircleYCoordinate), dots);
+            if (dot != null) {
+                dots.remove(dot);
+
+                mainCircleXCoordinate = dot.x();
+                mainCircleYCoordinate = dot.y();
+            } else {
+                Dot closestDot = DotManager.getClosestDot(new Dot(mainCircleXCoordinate, mainCircleYCoordinate), dots);
+                dots.remove(closestDot);
+
+                mainCircleXCoordinate = closestDot.x();
+                mainCircleYCoordinate = closestDot.y();
+            }
+        }
+
+        if (dots.size() > monthNumber) {
+            Dot dotWithLessNeighbors = DotManager.findDotWithLeastNeighbors(dots);
+            dots.remove(dotWithLessNeighbors);
+        }
+    }
+
+
     private void createNewDotInRandomCoordinate() {
-        List<int[]> newCoordinates = getNeighborCoordinates(mainCircleXCoordinate, mainCircleYCoordinate);
+        List<int[]> newCoordinates = DotManager.getNeighborCoordinates(mainCircleXCoordinate, mainCircleYCoordinate);
 
         newCoordinates = newCoordinates.stream()
                 .filter(coordinate -> dots.stream()
                         .noneMatch(dot -> Math.abs(dot.x() - coordinate[0]) < DOT_SIZE
                                 && Math.abs(dot.y() - coordinate[1]) < DOT_SIZE))
-                .collect(Collectors.toList());
+                .toList();
 
         if (!newCoordinates.isEmpty()) {
             int[] randomCoordinate = newCoordinates.get(random.nextInt(newCoordinates.size()));
@@ -160,27 +160,6 @@ public class Board extends JPanel implements ActionListener {
                 dots.add(newDot);
             }
         }
-    }
-
-    private List<int[]> filterValidCoordinates(List<int[]> coordinates, List<Dot> dots) {
-        return coordinates.stream()
-                .filter(coordinate -> dots.stream()
-                        .noneMatch(dot -> dot.x() == coordinate[0] && dot.y() == coordinate[1]))
-                .collect(Collectors.toList());
-    }
-
-    private List<int[]> getNeighborCoordinates(int x, int y) {
-        List<int[]> coordinates = new ArrayList<>();
-
-        int[] dx = { 1, -1, 0, 0, 1, -1, 1, -1 };
-        int[] dy = { 0, 0, 1, -1, 1, 1, -1, -1 };
-
-        for (int i = 0; i < 8; i++) {
-            int MOVE_AMOUNT = 10;
-            coordinates.add(new int[] { x + MOVE_AMOUNT * dx[i], y + MOVE_AMOUNT * dy[i] });
-        }
-
-        return coordinates;
     }
 
     private boolean isCloseToTheBorder() {
